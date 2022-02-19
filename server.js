831088c4c80bbf06b79db75af4a965b80b11fa02
Swapnil Kumar <swapnilkumar7663@gmail.com>
2022-02-19 00:30:10 -0800

    add cart page

--- server.js
+++ server.js
@@ -9,15 +9,27 @@ const PORT = process.env.PORT || 3300;
 //Assets
 app.use(express.static('public'));
 
-app.get('/',function(req,res){
-    res.render('home');
-})
 
 //set template engine
 
 app.use(expressLayout);
 app.set('views',path.join(__dirname,'/resources/views'));
 app.set('view engine','ejs');
+
+app.get('/',function(req,res){
+    res.render('home');
+});
+app.get('/cart',function(req,res){
+    res.render('customers/cart');
+});
+app.get('/login',function(req,res){
+    res.render('auth/login');
+});
+app.get('/register',function(req,res){
+    res.render('auth/register');
+})
+
+
 
 app.listen(PORT,function(){
     console.log(`Listening on port ${PORT}`);
